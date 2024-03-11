68a46c259a752d5aff2cf6eacd6d03c5cf3d91aa
Thomas Cazade <cazade.thomas@gmail.com>
2024-03-11 13:54:02 +0100

    chore(playground): improve UI

--- playground/Playground.vue
+++ playground/Playground.vue
@@ -10,21 +10,27 @@ const options = ref<string[]>([]);
 
 <template>
   <div class="container">
-    <VueSelect
-      v-model="option"
-      :is-multi="false"
-      :options="[
-        { label: 'Alice\'s Adventures in Wonderland', value: 'alice_in_wonderland', extra: 'hello' },
-        { label: 'A Wizard of Earthsea', value: 'wizard_earthsea', extra: 'world' },
-        { label: 'Harry Potter and the Philosopher\'s Stone', value: 'harry_potter_1997', extra: 'hw' },
-        { label: 'Harry Potter and the Chamber of Secrets', value: 'harry_potter_1998', extra: 'hw2' },
-        { label: 'Harry Potter and the Prisoner of Azkaban', value: 'harry_potter_1999', extra: 'hw3' },
-        { label: 'The Lord of the Rings', value: 'tlotr', extra: 'hw4' },
-        { label: 'The Hobbit', value: 'hobbit', extra: 'hw5' },
-        { label: 'The Silmarillion', value: 'silmarillion', extra: 'hw6' },
-      ]"
-      placeholder="Select a value"
-    />
+    <form class="form-container" @submit.prevent="null">
+      <VueSelect
+        v-model="option"
+        :is-multi="false"
+        :options="[
+          { label: 'Alice\'s Adventures in Wonderland', value: 'alice_in_wonderland', extra: 'hello' },
+          { label: 'A Wizard of Earthsea', value: 'wizard_earthsea', extra: 'world' },
+          { label: 'Harry Potter and the Philosopher\'s Stone', value: 'harry_potter_1997', extra: 'hw' },
+          { label: 'Harry Potter and the Chamber of Secrets', value: 'harry_potter_1998', extra: 'hw2' },
+          { label: 'Harry Potter and the Prisoner of Azkaban', value: 'harry_potter_1999', extra: 'hw3' },
+          { label: 'The Lord of the Rings', value: 'tlotr', extra: 'hw4' },
+          { label: 'The Hobbit', value: 'hobbit', extra: 'hw5' },
+          { label: 'The Silmarillion', value: 'silmarillion', extra: 'hw6' },
+        ]"
+        placeholder="Pick a book"
+      />
+
+      <p class="selected-value">
+        Selected value: {{ option || "none" }}
+      </p>
+    </form>
   </div>
 </template>
 
@@ -40,21 +46,24 @@ body {
 
 <style lang="scss" scoped>
 .container {
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  width: 100vw;
+  min-height: calc(100vh - 4rem);
+  padding: 2rem;
   background-color: #f4f4f5;
-}
 
-:deep(.vue-select) {
-  width: 100%;
-  max-width: 400px;
-}
+  .form-container {
+    display: flex;
+    flex-direction: column;
+    max-width: 400px;
+    padding: 2rem;
+    margin: 0 auto;
+    border-radius: 6px;
+    background-color: #fff;
 
-:deep(.vue-select .menu-option) {
-  background-color: red;
+    .selected-value {
+      font-size: 14px;
+      font-weight: 500;
+      font-family: "IBM Plex Mono", monospace;
+    }
+  }
 }
 </style>

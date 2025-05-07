aa09e87d30b2d25b3b1241bfe4181a3b04f876a5
Thomas Cazade <cazade.thomas@gmail.com>
2025-05-07 13:41:47 +0200

    fix(select): all slots should be optional for TS

--- src/types/slots.ts
+++ src/types/slots.ts
@@ -6,15 +6,15 @@ import type { Option } from "@/types/option";
  * @see https://vuejs.org/api/sfc-script-setup#defineslots
  */
 export type Slots<GenericOption extends Option<OptionValue>, OptionValue> = {
-  "value": (props: { option: GenericOption }) => any;
-  "tag": (props: { option: GenericOption; removeOption: () => void }) => any;
-  "clear": () => any;
-  "dropdown": () => any;
-  "loading": () => any;
-  "menu-header": () => any;
-  "option": (props: { option: GenericOption; index: number; isFocused: boolean; isSelected: boolean; isDisabled: boolean }) => any;
-  "no-options": () => any;
-  "taggable-no-options": () => any;
+  "value"?: (props: { option: GenericOption }) => any;
+  "tag"?: (props: { option: GenericOption; removeOption: () => void }) => any;
+  "clear"?: () => any;
+  "dropdown"?: () => any;
+  "loading"?: () => any;
+  "menu-header"?: () => any;
+  "option"?: (props: { option: GenericOption; index: number; isFocused: boolean; isSelected: boolean; isDisabled: boolean }) => any;
+  "no-options"?: () => any;
+  "taggable-no-options"?: () => any;
 };
 
 export type IndicatorsSlots<GenericOption extends Option<OptionValue>, OptionValue> = Pick<

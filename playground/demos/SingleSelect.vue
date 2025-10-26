d51d6037c6430437309cb04bc1b73a4c40fce97f
Thomas Cazade <cazade.thomas@gmail.com>
2025-10-26 09:48:40 +0100

    fix(select): improve logic to open/close menu when clicking on input or control (#352)
    * fix(select): improve logic to open/close menu when clicking on the input or select control
    
    * fix(playground): properly handle taggable by creating a new option

--- playground/demos/SingleSelect.vue
+++ playground/demos/SingleSelect.vue
@@ -5,12 +5,17 @@ import VueSelect from "../../src";
 
 const selected = ref<string | null>(null);
 
-const options: Option<string>[] = [
+const options = ref<Option<string>[]>([
   { label: "Alice's Adventures in Wonderland", value: "alice" },
   { label: "A Wizard of Earthsea", value: "wizard" },
   { label: "Harry Potter and the Philosopher's Stone", value: "harry_potter_1" },
   { label: "Harry Potter and the Chamber of Secrets", value: "harry_potter_2" },
-];
+]);
+
+const handleCreateOption = (value: string) => {
+  options.value.push({ label: value, value });
+  selected.value = value;
+};
 </script>
 
 <template>
@@ -20,6 +25,7 @@ const options: Option<string>[] = [
     :is-multi="false"
     :is-taggable="true"
     placeholder="Pick a book"
+    @option-created="(value) => handleCreateOption(value)"
   >
     <template #taggable-no-options="{ option }">
       <div class="custom-taggable-no-options">

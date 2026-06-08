14d4f6d882280a60543639c7970724f62d29cc39
Thomas Cazade <cazade.thomas@gmail.com>
2026-06-08 10:33:03 +0200

    build(playground): add playground environment

---
+++ playground/vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from "node:url";
+import vue from "unplugin-vue/vite";
+import { defineConfig } from "vite";
+
+const srcDir = fileURLToPath(new URL("../src", import.meta.url));
+
+export default defineConfig({
+  plugins: [vue()],
+
+  resolve: {
+    alias: {
+      "@": srcDir,
+      "vue3-select-component/primitives": `${srcDir}/primitives/index.ts`,
+      "vue3-select-component": `${srcDir}/index.ts`,
+    },
+  },
+
+  server: {
+    port: 5174,
+    open: true,
+  },
+});

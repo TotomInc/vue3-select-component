76444e817eb63b18ec23da9266874b80f8c9b093
Thomas Cazade <cazade.thomas@gmail.com>
2026-06-23 21:54:49 +0200

    docs: add link to bug repro

---
+++ docs/app/components/AppHeaderCTA.vue
@@ -0,0 +1,14 @@
+<template>
+  <UButton
+    label="Bug repro"
+    color="neutral"
+    variant="ghost"
+    to="https://play.vuejs.org/#eNqdU8Fu2zAM/RVBl6RAYnfoTpkbdBt6WA/bsPQ27eDZjOtUlgWJdlIY/vdSspy6RdoCvRjS0yP5+Eh3/KvWUdsAX/HEZqbUyCxgo5lMVXEpOFrB10KVla4Nso5tQEKGrGdbU1dMcAq9WFoPLrOaWAoUCj6JMLAd6TNiz4QSKquVRdamsgF26RiJRVOqYj2fzc6+jO+1xpIOA2P+VyhG6WT6H+SKKt+kbbrxigVfDLkcvCN4aERw1i9eBN0+aDgRhAQ/C/p3JlQSDxC1TxeESssUgW6MJcGGdlnVOUjyyaei4FUQTVA4ERj7FBSm10Mg5GPtrgs29H0Sa+Il8aQSXS0+SHe8CoY2Rs4Fv0PUdhXH+/0+apS+LyIyPz45jCuXy2Kcl/Tx2WyUWZLljKYeQ36+4EOFZZXqaGdrRSvROdUiPFAI6XWIw14ZvHNzFPeesE/ReXQ+6CpVDoeIlO2oih/aqzVCDx8uNbXAFeqF6ql5tLR027J40bpLUUowv8ZhTixIpaz3Nx5D08BRdnYH2f0JfGcPg+rfBiyYlvbl+IapKSD4d735CQe3oOMjLVkjif3G4x+wtWycxoH2rVE5yZ7wvNoffpL0p93a6wOCsmNTTqh3w/O99d/faP1J7kX0+ehi/whwAnj+"
+    target="_blank"
+    rel="noopener noreferrer"
+    size="sm"
+    class="hidden sm:inline-flex"
+    :external="true"
+    trailing-icon="i-lucide-external-link"
+  />
+</template>
